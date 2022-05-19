8a1ee8e7f066d952b8bf4b061fd7b7adb6846a05
Vladimir Louis <vovangus1235@gmail.com>
2022-05-19 19:46:48 +0300

    [Inital commit]

---
+++ droplane/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  droplane
+//
+//  Created by Vladimir Gusev on 19.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
